823d28563e14eb62742b8767937425ae48ca38e1
Gabriel Duarte <gabriel.anpedu@gmail.com>
2024-05-29 19:45:40 -0300

    Versão Base

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+pygame.init()
+tamanho = (800,600)
+clock = pygame.time.Clock()
+tela = pygame.display.set_mode( tamanho )
+pygame.display.set_caption("Iron Man do Marcão")
+branco = (255,255,255)
+
+while True:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            quit()
+
+    tela.fill(branco)
+    pygame.display.update()
+    clock.tick(60)
+pygame.quit()
